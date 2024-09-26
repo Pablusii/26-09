0b92a5acbf0dd1bb7214b19690b776a27d48ba82
Pablusii <sibaer17@gmail.com>
2024-09-26 14:55:59 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,22 @@
+name = ('Space')
+print (name)
+age=30
+print (age)
+new_age=32
+print (age + new_age)
+is_student = bool
+print (is_student == is_student)
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
